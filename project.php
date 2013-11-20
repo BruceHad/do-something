605115dd142aeb011b31c4bd17b013ab48c88b97
treerock <treerock@myopera.com>
2013-11-20 17:00:43 +0000

    Working on adding, removing and deleting tasks.

--- project.php
+++ project.php
@@ -10,20 +10,27 @@
     <div class="container ng-cloak">
         <div class="col col-3">
             <p>Add Task</p>
-            <form ng-submit="addTask()">
+            <form ng-submit="addTask(taskKey)">
                 <input type="text" value="" ng-model="newTask" placeholder="Add New Task" />
+                <div ng-class="{hidden: taskIsHidden()}">
+                    <input type="text" value="" ng-model="newDescription" placeholder="Add Description" />
+                    <input type="text" value="" ng-model="taskKey" placeholder="Add New Task" />
+                    <input type="text" value="" ng-model="newStartDate" placeholder="Change Start Date" />
+                    <input type="text" value="" ng-model="newEndDate" placeholder="Change End Date" />
+                </div>
+                <input type="submit" id="submit" value="Submit" />
             </form>
             <p>Select Date Range</p>
         </div>
         <div class="col col-9">
             <div class="day" ng-repeat="day in dates">
                 <h2>{{day}}</h2>
-                <table>
+                <table class="tasks">
                 <tr class="task" ng-repeat="(key,task) in tasks">
                     <td>{{key}}: {{task.name}}</td>
                     <td>{{task.description}}</td>
-                    <td>Edit</td>
-                    <td><a href="#" ng-click="delete(key)">Delete</a></td>
+                    <td><a href="" ng-click="edit(key)">Edit</td>
+                    <td><a href="" ng-click="delete(key)">Delete</a></td>
                 </tr>
                 </table>
             </div>
--- scripts/project.js
+++ scripts/project.js
@@ -2,20 +2,18 @@ function dateString(date) {
     date = new Date(date);
     return date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
 }
-
-
 Array.prototype.remove = function(from, to) {
-  var rest = this.slice((to || from) + 1 || this.length);
-  this.length = from < 0 ? this.length + from : from;
-  return this.push.apply(this, rest);
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
 };
 
 function ProjCntr($scope) {
     var dates = [];
-    var today = Date.now;
+    var today = dateString(Date.now());
     var days = 7;
     $scope.tasks = [];
-    $scope.newTask = "";
+    $scope.taskIsHidden = true;
     for(var i = 0; i < days; i++) {
         dates.push(new Date().setDate(new Date().getDate() + i));
     }
@@ -33,14 +31,40 @@ function ProjCntr($scope) {
         start_date: today,
         end_date: null
     });
-    $scope.addTask = function () {
-        $scope.tasks.push({
-            name: $scope.newTask,
-            start_date: today,
-            end_date: null});
-        $scope.newTask = "";
+    $scope.addTask = function(key) {
+        console.log(key);
+        if(typeof key != 'undefined') {
+            $scope.tasks[key].name = $scope.newTask;
+            $scope.tasks[key].description = $scope.newDescription;
+            $scope.tasks[key].start_date = $scope.newStartDate;
+            $scope.tasks[key].end_date = $scope.newEndDate;
+            $scope.taskIsHidden = true;
+            $scope.newTask = "";
+            $scope.newDescription = "";
+            $scope.newStartDate = "";
+            $scope.newEndDate = "";
+            $scope.taskKey = "";
+        } else {
+            $scope.tasks.push({
+                name: $scope.newTask,
+                start_date: today,
+                end_date: null
+            });
+            $scope.newTask = "";
+        }
     };
-    $scope.delete = function(key){
+    $scope.delete = function(key) {
         $scope.tasks.remove(key);
+    };
+    $scope.edit = function(key) {
+        $scope.newTask = $scope.tasks[key].name;
+        $scope.newDescription = $scope.tasks[key].description;
+        $scope.newStartDate = $scope.tasks[key].start_date;
+        $scope.newEndDate = $scope.tasks[key].end_date;
+        $scope.taskKey = key;
+        $scope.taskIsHidden = false;
+    };
+    $scope.taskIsHidden = function() {
+        return $scope.taskIsHidden;
     };
 }

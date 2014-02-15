f91d71ecb035cb746ce614b92e273780571b19eb
treerock <treercock@myopera.com>
2014-02-15 11:59:21 +0000

    Re-build - merge boilerplate in

--- app/js/app.js
+++ app/js/app.js
@@ -8,9 +8,13 @@ angular.module('myApp', [
   'myApp.services',
   'myApp.directives',
   'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+])
+// disabling routing as it isn't playing nicely
+// therefore partials are disabled
+// and I need to define the controller in the template.
+// .config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+//   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }])
+;
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,12 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope', function($scope) {
+    // Instantiate an object to store your scope data in (Best Practices)
+    $scope.data = {};
+    $scope.data.name = "Hello World";
   }])
-  .controller('MyCtrl2', [function() {
+  // .controller('MyCtrl2', [function() {
 
-  }]);
+  // }])
+;
